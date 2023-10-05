00e202a6918a6f11b9859d26ed18b01e4a865171
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2023-10-05 13:56:33 +0300

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,6 @@ const model = {
   ],
 };
 
-interface Param {
-  id: number;
-  name: string;
-  type: "string";
-}
-
 interface ParamValue {
   paramId: number;
   value: string;
@@ -41,59 +35,60 @@ interface Model {
   paramValues: ParamValue[];
 }
 
-interface Props {
-  params: Param[];
-  model: Model;
-}
-
 function App() {
+  const [paramValues, setParamValues] = useState<Map<number, string>>(
+    new Map()
+  );
+
+  useEffect(() => {
+    const initialValues = new Map<number, string>();
+    params.forEach((param) => {
+      const paramValue = model.paramValues.find(
+        (pv) => pv.paramId === param.id
+      );
+      initialValues.set(param.id, paramValue ? paramValue.value : "");
+    });
+    setParamValues(initialValues);
+  }, []);
+
+  // Обновляем значение параметра в состоянии при изменении ввода
+  const handleParamChange = (paramId: number, value: string) => {
+    setParamValues((prevParamValues) => {
+      const newParamValues = new Map(prevParamValues);
+      newParamValues.set(paramId, value);
+      return newParamValues;
+    });
+  };
+
+  // Получаем полную структуру Model с обновленными значениями
+  const getModel = (): Model => {
+    const updatedParamValues: ParamValue[] = [];
+    params.forEach((param) => {
+      const value = paramValues.get(param.id) || "";
+      updatedParamValues.push({ paramId: param.id, value });
+    });
+    return { paramValues: updatedParamValues };
+  };
+
   return (
-   const [paramValues, setParamValues] = useState<Map<number, string>>(
-		new Map()
-	 );
-  
-	 useEffect(() => {
-		const initialValues = new Map<number, string>();
-		params.forEach(param => {
-		  const paramValue = model.paramValues.find(pv => pv.paramId === param.id);
-		  initialValues.set(param.id, paramValue ? paramValue.value : '');
-		});
-		setParamValues(initialValues);
-	 }, [params, model]);
-  
-	 // Обновляем значение параметра в состоянии при изменении ввода
-	 const handleParamChange = (paramId: number, value: string) => {
-		setParamValues(prevParamValues => {
-		  const newParamValues = new Map(prevParamValues);
-		  newParamValues.set(paramId, value);
-		  return newParamValues;
-		});
-	 };
-  
-	 // Получаем полную структуру Model с обновленными значениями
-	 const getModel = (): Model => {
-		const updatedParamValues: ParamValue[] = [];
-		params.forEach(param => {
-		  const value = paramValues.get(param.id) || '';
-		  updatedParamValues.push({ paramId: param.id, value });
-		});
-		return { paramValues: updatedParamValues };
-	 };
-  
-	 return (
-		<div>
-		  {params.map(param => (
-			 <div key={param.id}>
-				<label>{param.name}:</label>
-				<input
-				  type="text"
-				  value={paramValues.get(param.id) || ''}
-				  onChange={e => handleParamChange(param.id, e.target.value)}
-				/>
-			 </div>
-		  ))}
-		</div>
-	 );
+    <div>
+      {params.map((param) => (
+        <div key={param.id}>
+          <label>{param.name}:</label>
+          <input
+            type="text"
+            value={paramValues.get(param.id) || ""}
+            onChange={(e) => handleParamChange(param.id, e.target.value)}
+          />
+        </div>
+      ))}
+      <p>Обновленные параметры товара:</p>
+      <ul>
+        {getModel().paramValues.map(({ paramId, value }) => (
+          <li key={paramId}>{value}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
