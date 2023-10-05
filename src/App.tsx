2728671953ff91cb2facf7e4f67350796932027d
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2023-10-05 14:15:27 +0300

    change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,87 @@
-import { useState, useEffect } from "react";
+import React, { Component } from "react";
+
+interface Param {
+  id: number;
+  name: string;
+  type: string;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface State {
+  paramValues: { [key: number]: string };
+}
+
+class ParamEditor extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    const paramValues: { [key: number]: string } = {};
+    props.params.forEach((param) => {
+      const paramValue = props.model.paramValues.find(
+        (pv) => pv.paramId === param.id
+      );
+      paramValues[param.id] = paramValue ? paramValue.value : "";
+    });
+
+    this.state = { paramValues };
+  }
+
+  handleParamChange = (paramId: number, value: string) => {
+    this.setState((prevState) => ({
+      paramValues: {
+        ...prevState.paramValues,
+        [paramId]: value,
+      },
+    }));
+  };
+
+  getModel = (): Model => {
+    const { params } = this.props;
+    const { paramValues } = this.state;
+
+    const updatedParamValues: ParamValue[] = [];
+    params.forEach((param) => {
+      const value = paramValues[param.id] || "";
+      updatedParamValues.push({ paramId: param.id, value });
+    });
+
+    return { paramValues: updatedParamValues };
+  };
+
+  render() {
+    const { params } = this.props;
+    const { paramValues } = this.state;
+    console.log(paramValues);
+
+    return (
+      <div>
+        {params.map((param) => (
+          <div key={param.id}>
+            <label>{param.name}:</label>
+            <input
+              type="text"
+              value={paramValues[param.id] || ""}
+              onChange={(e) => this.handleParamChange(param.id, e.target.value)}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 const params = [
   {
@@ -26,68 +109,10 @@ const model = {
   ],
 };
 
-interface ParamValue {
-  paramId: number;
-  value: string;
-}
-
-interface Model {
-  paramValues: ParamValue[];
-}
-
 function App() {
-  const [paramValues, setParamValues] = useState<Map<number, string>>(
-    new Map()
-  );
-
-  useEffect(() => {
-    const initialValues = new Map<number, string>();
-    params.forEach((param) => {
-      const paramValue = model.paramValues.find(
-        (pv) => pv.paramId === param.id
-      );
-      initialValues.set(param.id, paramValue ? paramValue.value : "");
-    });
-    setParamValues(initialValues);
-  }, []);
-
-  // Обновляем значение параметра в состоянии при изменении ввода
-  const handleParamChange = (paramId: number, value: string) => {
-    setParamValues((prevParamValues) => {
-      const newParamValues = new Map(prevParamValues);
-      newParamValues.set(paramId, value);
-      return newParamValues;
-    });
-  };
-
-  // Получаем полную структуру Model с обновленными значениями
-  const getModel = (): Model => {
-    const updatedParamValues: ParamValue[] = [];
-    params.forEach((param) => {
-      const value = paramValues.get(param.id) || "";
-      updatedParamValues.push({ paramId: param.id, value });
-    });
-    return { paramValues: updatedParamValues };
-  };
-
   return (
     <div>
-      {params.map((param) => (
-        <div key={param.id}>
-          <label>{param.name}:</label>
-          <input
-            type="text"
-            value={paramValues.get(param.id) || ""}
-            onChange={(e) => handleParamChange(param.id, e.target.value)}
-          />
-        </div>
-      ))}
-      <p>Обновленные параметры товара:</p>
-      <ul>
-        {getModel().paramValues.map(({ paramId, value }) => (
-          <li key={paramId}>{value}</li>
-        ))}
-      </ul>
+      <ParamEditor params={params} model={model} />
     </div>
   );
 }
